c269048d91292f3eb73df97eedbd5c27da9f41f2
CodeSandbox <support@codesandbox.io>
2025-02-12 04:06:45 +0000

    Fix: Remove duplicate repository declarations from build.gradle.kts to resolve Gradle build error

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,13 +17,6 @@ plugins {
     id("com.google.gms.google-services") version "4.4.0" apply false
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
