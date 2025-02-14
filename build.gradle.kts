ea2bde774d1aadc13f72e842fed74bcff4fb3ff0
CodeSandbox <support@codesandbox.io>
2025-02-14 20:47:03 +0000

    Update Gradle to 8.2, AGP to 8.2.0, and JDK to 21
    - Update Gradle wrapper to 8.2
    - Update Android Gradle Plugin to 8.2.0
    - Update JDK version to 21
    - Update dependencies to latest versions
    - Update Kotlin version to 1.9.20
    - Update Hilt version to 2.48
    - Update other dependencies to their latest stable versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id("com.android.application") version "7.3.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
-    id("com.google.dagger.hilt.android") version "2.45" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.5.3" apply false
-    id("com.google.gms.google-services") version "4.3.15" apply false
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.7.5" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
 
 tasks.register("clean", Delete::class) {
