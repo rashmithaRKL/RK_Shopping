f28e0a99b232be301b8b46d87d1e7e61fcf76398
CodeSandbox <support@codesandbox.io>
2025-02-23 18:12:56 +0000

    fix: update to stable AGP version 8.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.8.0" apply false
+    id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.dagger.hilt.android") version "2.50" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.7.6" apply false
