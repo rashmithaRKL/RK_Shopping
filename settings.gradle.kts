c4b264347bf415db3525c707a5d3a7f9c32683a5
CodeSandbox <support@codesandbox.io>
2025-02-23 18:36:10 +0000

    fix: update settings.gradle.kts with project-level configurations and jetifier

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,13 @@ dependencyResolutionManagement {
         mavenCentral()
         maven { url = uri("https://jitpack.io") }
     }
+    versionCatalogs {
+        create("libs") {
+            version("agp", "8.2.2")
+            version("kotlin", "1.9.22")
+            version("shimmer", "0.5.0")
+        }
+    }
 }
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
@@ -22,6 +29,8 @@ gradle.beforeProject {
     extra.apply {
         set("android.nonTransitiveRClass", true)
         set("android.nonFinalResIds", false)
+        set("android.useAndroidX", true)
+        set("android.enableJetifier", true)
     }
 }
 
