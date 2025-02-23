cf67e97aaed31fb7859a41831bc7afaab5c5f406
CodeSandbox <support@codesandbox.io>
2025-02-23 18:08:56 +0000

    chore: add version catalog and update build scripts for AGP 8.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,17 @@
-plugins {
-    id("com.android.application") version "8.8.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.google.dagger.hilt.android") version "2.50" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.7.6" apply false
-    id("com.google.gms.google-services") version "4.4.0" apply false
-}
-
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
         google()
         mavenCentral()
     }
+}
+
+plugins {
+    alias(libs.plugins.android.application) version "8.8.0" apply false
+    alias(libs.plugins.kotlin.android) version "1.9.22" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.7.6" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
 
 allprojects {
